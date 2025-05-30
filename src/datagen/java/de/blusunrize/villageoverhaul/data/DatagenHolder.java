7f6b309ef8719002f1d48d842673ad47271b949c
BluSunrize <danny.rad10@yahoo.de>
2025-05-30 23:03:18 +0200

    Use structure modifiers to populate woodland mansions with more pillagers

---
+++ src/datagen/java/de/blusunrize/villageoverhaul/data/DatagenHolder.java
@@ -0,0 +1,50 @@
+package de.blusunrize.villageoverhaul.data;
+
+import com.google.common.collect.ImmutableList;
+import de.blusunrize.villageoverhaul.VillageOverhaul;
+import net.minecraft.core.HolderGetter;
+import net.minecraft.core.HolderSet;
+import net.minecraft.core.RegistrySetBuilder;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.level.biome.MobSpawnSettings.SpawnerData;
+import net.minecraft.world.level.levelgen.structure.BuiltinStructures;
+import net.minecraft.world.level.levelgen.structure.Structure;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.common.world.StructureModifier;
+import net.neoforged.neoforge.common.world.StructureModifiers.AddSpawnsStructureModifier;
+import net.neoforged.neoforge.data.event.GatherDataEvent;
+import net.neoforged.neoforge.registries.NeoForgeRegistries.Keys;
+
+@EventBusSubscriber(bus = EventBusSubscriber.Bus.MOD, modid = VillageOverhaul.MODID)
+public class DatagenHolder
+{
+	public static final ResourceKey<StructureModifier> WOODLAND_MANSION = ResourceKey.create(
+			Keys.STRUCTURE_MODIFIERS,
+			ResourceLocation.fromNamespaceAndPath(VillageOverhaul.MODID, "woodland_mansion_illagers")
+	);
+
+	@SubscribeEvent
+	public static void gatherData(GatherDataEvent event)
+	{
+		if(event.includeServer())
+		{
+			event.createDatapackRegistryObjects(new RegistrySetBuilder().add(
+					Keys.STRUCTURE_MODIFIERS, bootstrap -> {
+						// Static registries only need to be looked up if you need to grab the tag data.
+						HolderGetter<Structure> structures = bootstrap.lookup(Registries.STRUCTURE);
+						bootstrap.register(WOODLAND_MANSION, new AddSpawnsStructureModifier(
+								HolderSet.direct(structures.get(BuiltinStructures.WOODLAND_MANSION).get()),
+								ImmutableList.of(
+										new SpawnerData(EntityType.PILLAGER, 2, 2, 3),
+										new SpawnerData(EntityType.VINDICATOR, 2, 1, 2),
+										new SpawnerData(EntityType.ILLUSIONER, 2, 1, 1),
+										new SpawnerData(EntityType.WITCH, 1, 1, 1)
+								)));
+					}));
+		}
+	}
+}
